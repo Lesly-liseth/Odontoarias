8b2bd7cc703e5717bb442fc5b2aab7b1185eca4d
unknown <leslyherrera851@gmail.com>
2023-08-21 09:33:14 -0500

    rutas

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://localhost'),
+    'url' => env('APP_URL', 'https://odontoarias-fronted-production.up.railway.app/'),
 
     'asset_url' => env('ASSET_URL'),
 
