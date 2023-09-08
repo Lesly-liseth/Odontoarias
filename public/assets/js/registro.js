106d0853377d6990b65abba8c2025a67f57272bb
unknown <leslyherrera851@gmail.com>
2023-09-07 21:41:56 -0500

    validar campos

--- public/assets/js/registro.js
+++ public/assets/js/registro.js
@@ -182,7 +182,7 @@ document.querySelector('.btn-registro').addEventListener('click', async function
   }
 
   function validateAddress(address) {
-    const addressRegex = /^[añÑ-zA-Z0-9\- ]+$/;
+    const addressRegex = /^[a-zA-ZñÑ0-9\- ]+$/;
     return addressRegex.test(address);
   }
 
